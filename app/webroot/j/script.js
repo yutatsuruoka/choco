89dc8e7a5524f8cd2a4c6185ef63cd1c01e4617d
Yuta Tsuruoka <yuta@tsuruoka.me>
2012-02-03 06:18:51 +0900

    lightbox追加

--- app/webroot/j/script.js
+++ app/webroot/j/script.js
@@ -44,3 +44,23 @@ $(".paypalLink").hover(
         function(){ $(this).css("opacity", .6); },
         function(){ $(this).css("opacity", 1); }
 );
+
+
+//colorbox 規約部分
+$(function(){
+    $(".inline").colorbox({inline:true, width:"60%", height:"70%"});
+    $(".callbacks").colorbox({
+        onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+        onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+        onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+        onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+        onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+    });
+
+    //Example of preserving a JavaScript event for inline calls.
+    $("#click").click(function(){ 
+        $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+        return false;
+    });
+});
+
