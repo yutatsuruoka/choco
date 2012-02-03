602aac2a701b87b5d724db1f4c80b21cb642c0f8
Antonio Kamiya <antonio.kamiya@gmail.com>
2012-02-03 11:56:55 +0900

    set live twitter info

--- app/Controller/Component/oauth_consumers/twitter_consumer.php
+++ app/Controller/Component/oauth_consumers/twitter_consumer.php
@@ -3,8 +3,13 @@
 // app/controllers/components/oauth_consumers/twitter_consumer.php
 class TwitterConsumer extends AbstractConsumer {
     public function __construct() {
-        parent::__construct('kTWupQLy38Xsv1jwt2bA', 
-                'N9bqMdrvWNpYu0VFMPK3Ixk75ChAkQLiH3TUox38');
+// dev        
+//        parent::__construct('kTWupQLy38Xsv1jwt2bA', 
+//                'N9bqMdrvWNpYu0VFMPK3Ixk75ChAkQLiH3TUox38');
+        
+// live
+        parent::__construct('Pwx4p6As05HSX0lGEABIug', 
+                'w21UAGR38itTae68fg5Jl5inFgVG4oAaNuaqX5ljo');
     }
 }
 
