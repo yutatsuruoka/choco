a15529846bce0fe3be23c2700f42a665e6a8f9f2
Yuta Tsuruoka <yuta@tsuruoka.me>
2012-02-06 23:11:49 +0900

    バグ修正

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -1,6 +1,8 @@
 <?php
 class PagesController extends AppController {
 	
+	var $components = array('Qdmail');
+	
     function beforeFilter() {
         parent::beforeFilter();
 
@@ -13,4 +15,26 @@ class PagesController extends AppController {
     function index() {}
 
     function no() {}
+    
+    function mailtest() {
+    	$mail = new QdmailComponent();
+   		$mail -> smtp(true);
+    　	$param = array(
+        'host'=>'mail42.heteml.jp',
+        'port'=> 25 , //これはSMTPAuthの例。認証が必要ないなら　25　でＯＫ。
+        'from'=>'thankyou@chocokure.com',//　Return-path: になります。
+        'protocol'=>'SMTP',// 認証が必要ないなら、'SMTP'
+        'user'=>'thankyou@chocokure.com', //SMTPサーバーのユーザーID
+        'pass' => 'choco1228', //SMTPサーバーの認証パスワード
+        );
+    
+    	$this->Qdmail->to('yuta@tsuruoka.me', '');
+    	$this->Qdmail->from('thankyou@chocokure.com');
+    	$this->Qdmail->subject('testTitle');
+    	$this->Qdmail->text( '本文をここにかきます' );
+		$this->Qdmail -> send();
+    }
+
+    
+    
 }
